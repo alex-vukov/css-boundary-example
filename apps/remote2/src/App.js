5fd10611b7f9ef5f08da2069142a61911a873844
alex-vukov <alexvukov@gmail.com>
2023-03-22 12:55:41 +0200

    Bump version of css-boundary

--- apps/remote2/src/App.js
+++ apps/remote2/src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { StrictMode, useEffect } from "react";
 import { CssBoundary } from "css-boundary";
 import "./styles.css";
 
 const App = () => {
   return (
-    <CssBoundary>
-      <div>
-        <h1>Remote application 2 - React Version {React.version}</h1>
-      </div>
-    </CssBoundary>
+    <StrictMode>
+      <CssBoundary>
+        <div>
+          <h1>Remote application 2 - React Version {React.version}</h1>
+        </div>
+      </CssBoundary>
+    </StrictMode>
   );
 };
 
